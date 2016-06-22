802b366aa45cde40d52538a6125cfa10de9e7c11
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-06-22 18:34:55 -0300

    general adjusts

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,25 +1,10 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, $http) {
+angular.module("contacts").controller("listaTelefonicaCtrl", function($scope, $http) {
 	$scope.app = 'Lista Telefonica';
-	// $scope.contacts = [
-	// 	{nome: "Pedro", telefone: "99999-9999", data: new Date(), cor: "blue", company: {nome: 'Oi', codigo: 14, categoria: 'Celular'}},
-	// 	{nome: "Ana", telefone: "99706-9102", data: new Date(), cor: "yellow", company: {nome: 'Oi', codigo: 14, categoria: 'Celular'}},
-	// 	{nome: "Maria", telefone: "98871-1268", data: new Date(), cor: "black", company: {nome: 'GVT', codigo: 25, categoria: 'Fixo'}}
-	// ];
-
-	// $scope.companies = [
-	// 	{nome: 'Oi', codigo: 14, categoria: 'Celular', preco: 2},
-	// 	{nome: 'Vivo', codigo: 15, categoria: 'Celular', preco: 1},
-	// 	{nome: 'Tim', codigo: 41, categoria: 'Celular', preco: 3},
-	// 	{nome: 'GVT', codigo: 25, categoria: 'Fixo', preco: 4},
-	// 	{nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 1}
-	// ];
 
 	$scope.contacts = []
 	$scope.companies = []
 
-	$scope.classe1 = 'selecionado';
-	$scope.classe2 = 'negrito';
-	$scope.criterioDeOrdenacao = 'nome';
+	$scope.sortOrder = 'name';
 
 	var loadContacts = function() {
 		$http.get("http://localhost:3000/contacts").success(function(data) {
@@ -45,26 +30,26 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
 
 	$scope.apagarContatos = function(contacts) {
 		$scope.contacts = contacts.filter(function(contact) {
-			if(!contact.selecionado) return contact;
+			if(!contact.selected) return contact;
 		});
 	}
 
 	$scope.isContatoSelecionado = function(contacts) {
 		return contacts.some(function(contact) {
-			return contact.selecionado;
+			return contact.selected;
 		});
 	}
 
 	$scope.selecionarTodos = function(check, contacts) {
 		$scope.contacts = contacts.filter(function(contact) {
-			contact.selecionado = check;
+			contact.selected = check;
 			return contact;
 		});
 	}
 
-	$scope.ordernarPor = function(campo) {
-		$scope.criterioDeOrdenacao = campo;
-		$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+	$scope.ordernarPor = function(field) {
+		$scope.sortOrder = field;
+		$scope.directionOrder = !$scope.directionOrder;
 	}
 
 	loadContacts();
